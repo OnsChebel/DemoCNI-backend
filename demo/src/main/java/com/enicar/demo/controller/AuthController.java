f512c7801d5362e4b00d4bcb56d78c437e4a24a8
OnsChebel <cheblnousa@gmail.com>
2026-07-18 10:29:16 +0100

    DTOs for admin added and AuthController edited

--- demo/src/main/java/com/enicar/demo/controller/AuthController.java
+++ demo/src/main/java/com/enicar/demo/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.enicar.demo.controller;
 
+import com.enicar.demo.dto.LoginRequestDTO;
+import com.enicar.demo.dto.AuthResponseDTO;
 import com.enicar.demo.security.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,28 +25,20 @@ public class AuthController {
     private JwtUtils jwtUtils;
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<?> login(@RequestBody LoginRequestDTO loginRequest) {
         try {
             authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(loginRequest.getLogin(), loginRequest.getPassword())
             );
-
             String token = jwtUtils.generateToken(loginRequest.getLogin());
-            return ResponseEntity.ok(Map.of("token", token));
-
-        } catch (AuthenticationException e) {
+            AuthResponseDTO response = AuthResponseDTO.builder()
+                    .token(token)
+                    .build();
+            return ResponseEntity.ok(response);
+        }
+        catch (AuthenticationException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                     .body(Map.of("message", "Identifiants invalides"));
         }
-    }
-
-    public static class LoginRequest {
-        private String login;
-        private String password;
-
-        public String getLogin() { return login; }
-        public void setLogin(String login) { this.login = login; }
-        public String getPassword() { return password; }
-        public void setPassword(String password) { this.password = password; }
     }
 }
